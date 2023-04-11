41c67f43b2ac0b3b64d399e01f90609efd2e4fff
Federico Gemmo <federicogemmo@gmail.com>
2023-04-11 12:18:29 +0200

    added agents

---
+++ pandasagent.py
@@ -0,0 +1,8 @@
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.llms import OpenAI
+import pandas as pd
+
+df = pd.read_csv('titanic.csv')
+agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True)
+agent.run("how many people have more than 3 sibligngs")
+
